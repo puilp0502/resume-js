9a96280fe83b54dcf9c195ca54465d1eac0114b2
Frank Yang <puilp0502@gmail.com>
2020-09-07 02:18:07 +0900

    App: Implement main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import { Profile, ProfileProps } from "./components/profile";
+import {
+  ExperienceSection,
+  ExperienceSectionProps,
+} from "./components/experience";
+import { SkillSection, SkillSectionProps } from "./components/skill";
+
+import "./App.css";
+
+type SectionData =
+  | {
+      type: "profile";
+      data: ProfileProps;
+    }
+  | {
+      type: "experience";
+      data: ExperienceSectionProps;
+    }
+  | {
+      type: "skill";
+      data: SkillSectionProps;
+    };
+
+function SectionRenderer({ data }: { data: SectionData }) {
+  switch (data.type) {
+    case "profile":
+      return <Profile {...data.data} />;
+    case "experience":
+      return <ExperienceSection {...data.data} />;
+    case "skill":
+      return <SkillSection {...data.data} />;
+  }
+}
+
+const Container = styled.div`
+  @media screen and (max-width: 800px) {
+    width: 96vw;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 960px;
+  }
+  @media screen and (min-width: 800px) and (max-width: 1200px) {
+    width: 80vw;
+  }
+  margin: auto;
+`;
+/// Temporary data ingest method
+import resumeData from "./resume.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const sections = resumeData.sections.map((v, idx) => (
+    <SectionRenderer data={v as SectionData} key={idx} />
+  ));
+  return <Container>{sections}</Container>;
 }
 
 export default App;
